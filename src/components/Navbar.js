cb4175d230052bbb1fa7ee4b733480a7a1e12b1d
aniket-6234 <aniketashok1999@gmail.com>
2022-04-10 08:13:03 +0530

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="h-16 w-full top-0 left-0 right-0 bg-gray-200 shadow-sm fixed flex items-center">
+      <div className="text-white flex items-center justify-center h-12 px-5 rounded-lg bg-black lg:mx-36 mx-8">NEWS</div>
+    </nav>
+  )
+}
+
+export default Navbar
